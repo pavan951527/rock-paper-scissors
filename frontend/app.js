d597c9df8e8ab79b8d2b81420450ec06f5438fee
unknown <pavansingireddy147@gmail.com>
2021-06-22 14:20:50 +0530

    add

--- frontend/app.js
+++ frontend/app.js
@@ -27,7 +27,7 @@ const startGame = (humanChoice)=>{
       result.innerHTML = `<h2 style = "color:green">You won</h2>`          
 	mainDiv.innerHTML = `
 	    <div><h2>You</h2><img src='https://media.giphy.com/media/icIr8rk03xQI6NS67S/giphy.gif'><h2>${humanChoice}</h2></div>
-	    <div></h2>Opponent</h2><img src='https://media.giphy.com/media/QaS3chN1PNKEqUcVKS/giphy.gif'><h2>${botChoice}</h2></div>
+	    <div><h2>Opponent</h2><img src='https://media.giphy.com/media/QaS3chN1PNKEqUcVKS/giphy.gif'><h2>${botChoice}</h2></div>
 	    `
 	}
 
