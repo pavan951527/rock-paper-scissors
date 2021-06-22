954452c8e32b17f4df8e64961bd4f7b2bbe6dfd0
unknown <pavansingireddy147@gmail.com>
2021-06-22 14:06:49 +0530

    add

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const app = express();
 
-
+app.use(express.static('frontend'));
 
 app.get("/" ,function(req , res){
-    res.sendFile(__dirname+"/app.html");
+    res.sendFile(__dirname+"/frontend/app.html");
+    
 })
 var port  = process.env.PORT || 3000;
 
